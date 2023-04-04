4296f2e5cd4a202fe29e506add8b9ee3e146bc0c
Jorge Mayoral Álvarez <jorge.ma@yorch.dev>
2023-04-04 19:24:53 +0200

    feat: add simple ui

---
+++ src/lib/ResponseText.svelte
@@ -0,0 +1,11 @@
+<p class="text-Text">
+  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nesciunt soluta cum,
+  voluptatem cumque similique, non quod omnis quibusdam sed accusamus aut
+  officiis asperiores esse? Corporis modi animi impedit accusamus maiores. Lorem
+  ipsum dolor sit amet consectetur adipisicing elit. Ad numquam similique ut id
+  eius tempora sunt modi ipsam, earum quae eaque vero enim, autem totam expedita
+  debitis error quod? Quisquam! Lorem ipsum dolor, sit amet consectetur
+  adipisicing elit. Illum velit consequatur repudiandae earum vel eius rem.
+  Nostrum tempore accusamus nihil vitae error! Dolores repellendus voluptatibus
+  officiis quod, perspiciatis quos quia.
+</p>
---
+++ src/lib/TextForm.svelte
@@ -0,0 +1,23 @@
+<form on:submit|preventDefault>
+  <div class="w-full mb-4 border rounded-lg bg-Surface1 border-Surface2">
+    <div class="px-4 py-2 rounded-t-lg bg-Surface0">
+      <label for="comment" class="sr-only">Your comment</label>
+      <textarea
+        id="comment"
+        rows="4"
+        class="w-full px-0 text-sm border-0 bg-Surface0 focus:ring-transparent text-Text placeholder-Subtext1"
+        placeholder="Write your question..."
+      />
+    </div>
+    <div
+      class="flex items-center justify-between px-3 py-2 border-t border-Surface2"
+    >
+      <button
+        type="submit"
+        class="inline-flex items-center py-2.5 px-4 text-xs font-medium text-center text-Surface0 bg-Sky rounded-lg focus:ring-transparent hover:bg-Blue"
+      >
+        Send Question
+      </button>
+    </div>
+  </div>
+</form>
